f0207fd3aa4d040e4ec9a565961c89b435d3292e
court-tek <dishoughng@gmail.com>
2019-02-27 23:27:13 -0800

    css updates

--- header.php
+++ header.php
@@ -10,10 +10,25 @@
     <?php wp_head(); ?>
   </head>
   <body>
-    <header>
-      <h1>
-        autodidact<br>
-        programmer
-      </h1>
-    </header>
+    <section id="topImg">
+      <div class="title-area">
+        <h1>
+          the<br>
+          autodidact<br>
+          programmer
+        </h1>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      <div class="social-media">
+        <div class="rotate-this">
+          <a href="#">Twitter</a>
+          <a href="#">Instagram</a>
+          <a href="#">email</a>
+        </div>
+      </div>
+    </section>
     <div class="customContainer">
