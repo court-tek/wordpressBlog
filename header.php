302960e87159e26d10b846c01fd40f1d3022a650
court-tek <dishoughng@gmail.com>
2019-02-28 23:19:20 -0800

    updated css

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <title>PHP Isn't Dead</title>
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous"> -->
     <!-- stylesheet -->
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen,projection" />
     <?php wp_head(); ?>
@@ -12,18 +13,23 @@
   <body>
     <section id="topImg">
       <div class="title-area">
+        <div class="menu-area">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
         <h1>
           the<br>
           autodidact<br>
           programmer
         </h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
       </div>
       <div class="social-media">
+          <div class="search-area">
+            <i class="fas fa-search"></i>
+          </div>
         <div class="rotate-this">
           <a href="#">Twitter</a>
           <a href="#">Instagram</a>
